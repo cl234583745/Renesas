eb881e02846fdfd6791e5a0f6aaf90610de86730
jerryflpll <292812832@qq.com>
2022-10-31 22:35:15 +0800

    IAR&KEIL

---
+++ 3_IDE_IAR&MDK/start_ra2l1_iar/src/hal_entry.c
@@ -0,0 +1,197 @@
+/***********************************************************************************************************************
+ * Copyright [2020-2022] Renesas Electronics Corporation and/or its affiliates.  All Rights Reserved.
+ *
+ * This software and documentation are supplied by Renesas Electronics America Inc. and may only be used with products
+ * of Renesas Electronics Corp. and its affiliates ("Renesas").  No other uses are authorized.  Renesas products are
+ * sold pursuant to Renesas terms and conditions of sale.  Purchasers are solely responsible for the selection and use
+ * of Renesas products and Renesas assumes no liability.  No license, express or implied, to any intellectual property
+ * right is granted by Renesas. This software is protected under all applicable laws, including copyright laws. Renesas
+ * reserves the right to change or discontinue this software and/or this documentation. THE SOFTWARE AND DOCUMENTATION
+ * IS DELIVERED TO YOU "AS IS," AND RENESAS MAKES NO REPRESENTATIONS OR WARRANTIES, AND TO THE FULLEST EXTENT
+ * PERMISSIBLE UNDER APPLICABLE LAW, DISCLAIMS ALL WARRANTIES, WHETHER EXPLICITLY OR IMPLICITLY, INCLUDING WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NONINFRINGEMENT, WITH RESPECT TO THE SOFTWARE OR
+ * DOCUMENTATION.  RENESAS SHALL HAVE NO LIABILITY ARISING OUT OF ANY SECURITY VULNERABILITY OR BREACH.  TO THE MAXIMUM
+ * EXTENT PERMITTED BY LAW, IN NO EVENT WILL RENESAS BE LIABLE TO YOU IN CONNECTION WITH THE SOFTWARE OR DOCUMENTATION
+ * (OR ANY PERSON OR ENTITY CLAIMING RIGHTS DERIVED FROM YOU) FOR ANY LOSS, DAMAGES, OR CLAIMS WHATSOEVER, INCLUDING,
+ * WITHOUT LIMITATION, ANY DIRECT, CONSEQUENTIAL, SPECIAL, INDIRECT, PUNITIVE, OR INCIDENTAL DAMAGES; ANY LOST PROFITS,
+ * OTHER ECONOMIC DAMAGE, PROPERTY DAMAGE, OR PERSONAL INJURY; AND EVEN IF RENESAS HAS BEEN ADVISED OF THE POSSIBILITY
+ * OF SUCH LOSS, DAMAGES, CLAIMS OR COSTS.
+ **********************************************************************************************************************/
+
+#include "hal_data.h"
+
+void R_BSP_WarmStart(bsp_warm_start_event_t event);
+
+extern bsp_leds_t g_bsp_leds;
+
+
+#ifndef PRINTF
+#define PRINTF
+#endif
+#ifdef PRINTF
+#include <stdio.h>
+/**
+ * notice: g_uart0CB; g_uart0_ctrl
+ *
+ * e2s:
+ * 1.uart0 callback:g_uart0CB
+ * 2.FSP-BSP-heap size:0x400
+ * 3.-u _printf_float
+ * 4.other link void
+ *
+ * iar:
+ * 1.uart0 callback:g_uart0CB
+ * 2.FSP-BSP-heap size:0x400
+ * 3.libray=full
+ * 4.Semihosted=None
+ *
+ * keil:
+ * 1.uart0 callback:g_uart0CB
+ * 2.FSP-BSP-heap size:0x400
+ */
+volatile bool uart_send_complete_flag = false;
+void g_uart0CB (uart_callback_args_t * p_args)
+{
+    if(p_args->event == UART_EVENT_TX_COMPLETE)
+    {
+        uart_send_complete_flag = true;
+    }
+}
+#if defined __GNUC__ && !defined __clang__
+int _write(int fd, char *pBuffer, int size); //??????
+int _write(int fd, char *pBuffer, int size)
+{
+   (void)fd;
+   fsp_err_t err = R_SCI_UART_Write(&g_uart0_ctrl, (uint8_t *)pBuffer, (uint32_t)size);
+   if(FSP_SUCCESS != err) __BKPT();
+   while(uart_send_complete_flag == false);
+   uart_send_complete_flag = false;
+
+   return size;
+}
+#else
+int fputc(int ch, FILE *f)
+{
+   (void)f;
+   fsp_err_t err = R_SCI_UART_Write(&g_uart0_ctrl, (uint8_t *)&ch, 1);
+   if(FSP_SUCCESS != err) __asm("bkpt 0");
+   while(uart_send_complete_flag == false);
+   uart_send_complete_flag = false;
+
+   return ch;
+}
+#endif//#if defined __GNUC__ && !defined __clang__
+#endif//PRINTF
+/*******************************************************************************************************************//**
+ * @brief  Blinky example application
+ *
+ * Blinks all leds at a rate of 1 second using the software delay function provided by the BSP.
+ *
+ **********************************************************************************************************************/
+void hal_entry (void)
+{
+#if BSP_TZ_SECURE_BUILD
+
+    /* Enter non-secure code */
+    R_BSP_NonSecureEnter();
+#endif
+
+    
+#ifdef PRINTF
+    /* Open the transfer instance with initial configuration. */
+    fsp_err_t err = R_SCI_UART_Open(&g_uart0_ctrl, &g_uart0_cfg);
+    assert(FSP_SUCCESS == err);
+
+    printf("date:%s\ntime:%s\nfile:%s\nfunc:%s,line:%d\nhello world!\n", __DATE__, __TIME__, __FILE__, __FUNCTION__, __LINE__);
+
+    float PI = 3.1415926;
+    printf("PI=%f\n", PI);
+#endif//PRINTF
+    
+    /* Define the units to be used with the software delay function */
+    const bsp_delay_units_t bsp_delay_units = BSP_DELAY_UNITS_MILLISECONDS;
+
+    /* Set the blink frequency (must be <= bsp_delay_units */
+    const uint32_t freq_in_hz = 2;
+
+    /* Calculate the delay in terms of bsp_delay_units */
+    const uint32_t delay = bsp_delay_units / freq_in_hz;
+
+    /* LED type structure */
+    bsp_leds_t leds = g_bsp_leds;
+
+    /* If this board has no LEDs then trap here */
+    if (0 == leds.led_count)
+    {
+        while (1)
+        {
+            ;                          // There are no LEDs on this board
+        }
+    }
+
+    /* Holds level to set for pins */
+    bsp_io_level_t pin_level = BSP_IO_LEVEL_LOW;
+
+    while (1)
+    {
+        /* Enable access to the PFS registers. If using r_ioport module then register protection is automatically
+         * handled. This code uses BSP IO functions to show how it is used.
+         */
+        R_BSP_PinAccessEnable();
+
+        /* Update all board LEDs */
+        for (uint32_t i = 0; i < leds.led_count; i++)
+        {
+            /* Get pin to toggle */
+            uint32_t pin = leds.p_leds[i];
+
+            /* Write to this pin */
+            R_BSP_PinWrite((bsp_io_port_pin_t) pin, pin_level);
+        }
+
+        /* Protect PFS registers */
+        R_BSP_PinAccessDisable();
+
+        /* Toggle level for next write */
+        if (BSP_IO_LEVEL_LOW == pin_level)
+        {
+            pin_level = BSP_IO_LEVEL_HIGH;
+        }
+        else
+        {
+            pin_level = BSP_IO_LEVEL_LOW;
+        }
+
+        /* Delay */
+        R_BSP_SoftwareDelay(delay, bsp_delay_units);
+    }
+}
+
+/*******************************************************************************************************************//**
+ * This function is called at various points during the startup process.  This implementation uses the event that is
+ * called right before main() to set up the pins.
+ *
+ * @param[in]  event    Where at in the start up process the code is currently at
+ **********************************************************************************************************************/
+void R_BSP_WarmStart (bsp_warm_start_event_t event)
+{
+    if (BSP_WARM_START_RESET == event)
+    {
+#if BSP_FEATURE_FLASH_LP_VERSION != 0
+
+        /* Enable reading from data flash. */
+        R_FACI_LP->DFLCTL = 1U;
+
+        /* Would normally have to wait tDSTOP(6us) for data flash recovery. Placing the enable here, before clock and
+         * C runtime initialization, should negate the need for a delay since the initialization will typically take more than 6us. */
+#endif
+    }
+
+    if (BSP_WARM_START_POST_C == event)
+    {
+        /* C runtime environment and system clocks are setup. */
+
+        /* Configure pins. */
+        R_IOPORT_Open(&g_ioport_ctrl, g_ioport.p_cfg);
+    }
+}
