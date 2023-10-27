9619167294b64525b430740d2b007e07839179e3
jerryflpll <292812832@qq.com>
2023-10-27 20:02:35 +0800

    rzn2l Ethernet change files

--- RZN2L/6_EthernetIP/RZN2L_OpENer_RSK_rev0101/project/rzn2l_rsk/opener_single/e2studio/rzn_cfg/fsp_cfg/r_ether_selector_cfg.h
+++
@@ -1,6 +0,0 @@
-/* generated configuration header file - do not edit */
-#ifndef R_ETHER_SELECTOR_CFG_H_
-#define R_ETHER_SELECTOR_CFG_H_
-#define ETHER_SELECTOR_CFG_PARAM_CHECKING_ENABLE (BSP_CFG_PARAM_CHECKING_ENABLE)
-#define ETHER_SELECTOR_CFG_MODE       (0)
-#endif /* R_ETHER_SELECTOR_CFG_H_ */
